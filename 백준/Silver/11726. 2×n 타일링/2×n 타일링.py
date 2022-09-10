22752fcf2fe74d1bfafb3e63902c21486e1601e3
mintjamong <107983908+mintjamong@users.noreply.github.com>
2022-09-10 12:11:17 +0900

    [Silver III] Title: 2×n 타일링, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -4,8 +4,8 @@ d = [0] * 1001
 
 d[1] = 1
 d[2] = 2
-
+# overflow 날 수 있어서 미리 나누기
 for i in range(3, n+1):
-    d[i] = d[i-1]+d[i-2]
+    d[i] = (d[i-1]+d[i-2])%10007
 
-print(d[n]%10007)
+print(d[n])
